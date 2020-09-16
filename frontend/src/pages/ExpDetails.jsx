4159d1c5fae7b6caee0d03f1032477a34e36c553
Liel Malihi <lielm1995@gmail.com>
2020-09-16 20:31:34 +0300

    exp-edit scss & show images

--- frontend/src/pages/ExpDetails.jsx
+++ frontend/src/pages/ExpDetails.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import { expService } from '../services/expService';
 
 class _ExpDetails extends Component {
 
@@ -8,15 +9,23 @@ class _ExpDetails extends Component {
     }
 
     async componentDidMount() {
-        console.log(this.props.match.params.id);
-        // const exp = expSe
+        const id = this.props.match.params.id;
+        const exp = await expService.getById(id);
+        if(!exp) return;
+        this.setState({exp}, () => console.log(this.state.exp))
     }
     
 
     render() {
+        const {exp} = this.state;
+        if (!exp) return <div> </div>
         return (
-            <div>
-               Details 
+            <div className="exp-details">
+                <section className="exp-img">
+                    {
+                        exp.imgUrls.map((imgUrl, idx) => <img key={`img-${idx}-${exp._id}`} src={imgUrl} alt="img" />)
+                    }
+                </section>
             </div>
         )
     }
@@ -28,7 +37,7 @@ const mapStateToProps = state => {
     };
   };
   const mapDispatchToProps = {
-    // getById
+    
   };
   
   export const ExpDetails =  connect(mapStateToProps, mapDispatchToProps)(_ExpDetails);
