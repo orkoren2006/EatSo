ca16baca18756409f54a3cdcd8d0bd59f32bbc7e
Liel Malihi <lielm1995@gmail.com>
2020-09-16 15:28:08 +0300

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -36,46 +36,7 @@ class _Home extends Component {
   render() {
     return (
       <div className="home">
-        {this.props.reviews && <ul>
-          {this.props.reviews.map(review => (
-            <li key={review._id}>
-              <h3>{review.txt}</h3>
-              <p>
-                <Link to={`user/${review.aboutUser._id}`}>
-                  About {review.aboutUser.username}
-                </Link>
-              </p>
-              <p>
-                <Link to={`user/${review.byUser._id}`}>
-                  By {review.byUser.username}
-                </Link>
-              </p>
-              <hr />
-            </li>
-          ))}
-        </ul>}
-        {this.props.users && this.props.loggedInUser &&
-          <form onSubmit={this.addReview}>
-            <select
-              onChange={this.handleChange}
-              value={this.state.reviewToEdit.aboutUserId}
-              name="aboutUserId"
-            >
-              <option value="">Select User</option>
-              {this.props.users.map(user => (
-                <option key={user._id} value={user._id}>
-                  {user.username}
-                </option>
-              ))}
-            </select>
-            <textarea
-              name="txt"
-              onChange={this.handleChange}
-              value={this.state.reviewToEdit.txt}
-            ></textarea>
-            <button>Submit</button>
-          </form>}
-        <hr />
+        Home!
       </div>
     );
   }
