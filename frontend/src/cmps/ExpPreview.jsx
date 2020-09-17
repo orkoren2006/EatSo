91028e7e6513387da272a44ab22044135e1c7e79
orkoren2006 <orkoren2006@gmail.com>
2020-09-17 12:22:56 +0300

    some css for preview

--- frontend/src/cmps/ExpPreview.jsx
+++ frontend/src/cmps/ExpPreview.jsx
@@ -2,16 +2,21 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 
-export function ExpPreview (props) {
+export function ExpPreview(props) {
     const { exp } = props
-        return (
-            <div className="exp-card">
-                <Link to={`/exp/${exp._id}`}>
-                <img src={exp.imgUrls[0]}></img>
+    return (
+        <div className="exp-card">
+            <Link to={`/exp/${exp._id}`}>
+                <section className="cont flex">
+                    <img className="img-in-div" src={exp.imgUrls[0]}></img>
+                    <div className="div-in-image">${exp.price}</div>
+                </section>
+                <desc className="desc">
                 <p><span>Dinner </span>in {exp.location.city}</p>   {/* replace the word 'dinner' with a varaible */}
                 <h3>{exp.name}</h3>
                 <p>{exp.reviews[0].rate}/5</p>
-                </Link>
-            </div>
-        )
-    }
+                </desc>
+            </Link>
+        </div>
+    )
+}
