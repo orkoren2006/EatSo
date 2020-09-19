90b79929c65cfe4b4885e625214424172375a7fe
orkoren2006 <orkoren2006@gmail.com>
2020-09-19 18:11:22 +0300

    some css

--- frontend/src/cmps/Header.jsx
+++ frontend/src/cmps/Header.jsx
@@ -19,21 +19,25 @@ export class Header extends Component {
 
     render() {
         const toggleNavbarClass = this.state.navbar ? 'main-nav flex toggle' : 'main-nav flex'
+        const toggleScreen = this.state.navbar ? 'header-screen' : 'header-screen open-menu'
         // const toggleNavbarButton = this.state.button ? 'main-nav-botton' : 'main-nav-botton-hide'
         return (
-            <div className="header flex align-center space-between">
-                <Link to="/"><h1>EatSo!</h1></Link>
-                <nav className={toggleNavbarClass}>
-                    <ul>
-                        <li><NavLink onClick={this.toggleMenu} to="/login">Login</NavLink></li>
-                        <li><NavLink onClick={this.toggleMenu} to="/">User Reviews</NavLink></li>
-                        <li><NavLink onClick={this.toggleMenu} to="/about">Chat Room</NavLink></li>
-                        <li><NavLink onClick={this.toggleMenu} to="/exp">Experiences</NavLink></li>
-                    </ul>
-                </nav>
-                <button onClick={this.toggleMenu}>X</button>
-                {/* <button className={toggleNavbarButton} onClick={this.toggleMenu}>X</button> */}
-            </div>
+            <React.Fragment>
+                <div className={toggleScreen} onClick={this.toggleMenu}></div>
+                <div className="header flex align-center space-between">
+                    <Link to="/"><h1>EatSo!</h1></Link>
+                    <nav className={toggleNavbarClass}>
+                        <ul>
+                            <li><NavLink onClick={this.toggleMenu} to="/login">Login</NavLink></li>
+                            <li><NavLink onClick={this.toggleMenu} to="/">User Reviews</NavLink></li>
+                            <li><NavLink onClick={this.toggleMenu} to="/about">Chat Room</NavLink></li>
+                            <li><NavLink onClick={this.toggleMenu} to="/exp">Experiences</NavLink></li>
+                        </ul>
+                    </nav>
+                    <img className="login-avatar" src="https://res.cloudinary.com/orkofy/image/upload/v1600527561/eatso-profile/img_avatar_rqu4ym.png" onClick={this.toggleMenu}></img>
+                    {/* <button className={toggleNavbarButton} onClick={this.toggleMenu}>X</button> */}
+                </div>
+            </React.Fragment>
         )
     }
 }
