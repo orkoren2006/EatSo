a85f6797ec578c1f4b562492cda9919a39af1d21
ItayAvrahami <itayavrahami1@gmail.com>
2020-09-23 09:08:50 +0300

    chch

--- frontend/src/pages/ExpApp.jsx
+++ frontend/src/pages/ExpApp.jsx
@@ -6,14 +6,10 @@ import { ExpList } from '../cmps/ExpList';
 class _ExpApp extends Component {
 
     async componentDidMount() {
-        // let { field, value } = this.props.match.params
-        // let field=null
-        // let value= null
         let filterBy;
         if (this.props.match.params.field) {filterBy = {[this.props.match.params.field]:this.props.match.params.value}}
         console.log(filterBy);
         await this.props.loadExps(filterBy);
-        // this.setState({exps}, () => console.log(this.state.exps))
     }
 
     render() {
