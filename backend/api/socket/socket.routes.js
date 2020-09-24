7ea183a1a432f811599cab50995b827dd9547c28
Liel Malihi <lielm1995@gmail.com>
2020-09-24 15:19:03 +0300

    f

--- backend/api/socket/socket.routes.js
+++ backend/api/socket/socket.routes.js
@@ -22,14 +22,18 @@ function connectSockets(io) {
         })
 
         socket.on('user notification', userId => {
-           
+            // console.log('userId', userId)
+            
             if (socket.userId) {
                 socket.leave(socket.userId)
             }
             socket.join(userId)
             socket.userId = userId;
         })
-        socket.on('booking exp', booking => { 
+        socket.on('booking exp', booking => {
+            // console.log('booked!')
+             console.log('socket.userId', socket.userId)
+             
             io.to(socket.userId).emit('new booking', booking)
         })
 
