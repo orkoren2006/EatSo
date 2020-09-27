6ca023e51a709dc3d7c9b0881fe2c4803d44869b
ItayAvrahami <itayavrahami1@gmail.com>
2020-09-27 18:33:47 +0300

    CHECK

--- frontend/src/cmps/Notification.jsx
+++ frontend/src/cmps/Notification.jsx
@@ -8,11 +8,12 @@ export class _Notification extends Component {
     getClass = (notification) => {
 
         let notificationType = '';
-        const notificationVisibility = (notification) ? 'visible' : 'hidden'
+        // const notificationVisibility = (notification) ? 'visible' : 'hidden'
+        const notificationVisibility = 'visible'
         if (Object.keys(notification).includes('isSuccessed')) {
             notificationType = (notification.isSuccessed) ? 'successed' : 'failed'
         }
-        return `user-msg ${notificationVisibility } ${notificationType}`
+        return `user-msg flex space-between column justify-center align-center ${notificationVisibility} ${notificationType}`
     }
     getMsg = (notification) => {
         const notificationMsg = (notification) ? notification.msg : ''
@@ -21,10 +22,17 @@ export class _Notification extends Component {
 
     render() {
         const notification = this.props.notification || ''
+        // return <div className="user-msg flex column justify-center align-center space-between">
+        //     <h4>Experience Updated</h4>
+        //     {/* The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog */}
+        //     <button>Close</button>
+        // </div>
         return notification &&
             <div className={this.getClass(notification)} >
                 <pre>{this.getMsg(notification)}</pre>
-                <button onClick={this.props.clearNotification}>Close</button>
+                <section className="flex align-center">
+                    <button onClick={this.props.clearNotification}>Close</button>
+                </section>
             </div>
     }
 }
