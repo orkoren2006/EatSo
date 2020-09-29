c966385af184073b76eecf8fbe5cbf136a9bdb44
ItayAvrahami <itayavrahami1@gmail.com>
2020-09-29 16:09:48 +0300

    PUSH

--- frontend/src/cmps/BookingList.jsx
+++ frontend/src/cmps/BookingList.jsx
@@ -6,17 +6,6 @@ import { BookingPreview } from './BookingPreview'
 export function BookingList({ bookings, onChangeStatusBooking }) {
     
     return (
-<<<<<<< HEAD
-        <div className="booking-list">
-            
-                <div style={{fontWeight:'bold'}}>Guest</div>
-                <div style={{fontWeight:'bold'}}>Quantity</div>
-                <div style={{fontWeight:'bold'}}>Experience Name</div>
-                <div style={{fontWeight:'bold'}}>Approve/Reject</div>
-            
-            {bookings.map(booking => <BookingPreview key={booking._id} booking={booking} onChangeStatusBooking={onChangeStatusBooking} />)}
-        </div>
-=======
         <section className="host-booking-queue">
             <h3>Booking Queue</h3>
             <div className="booking-list">
@@ -28,7 +17,6 @@ export function BookingList({ bookings, onChangeStatusBooking }) {
                 {bookings.map(booking => <BookingPreview key={booking._id} booking={booking} onChangeStatusBooking={onChangeStatusBooking} />)}
             </div>
         </section>
->>>>>>> 15e84935f8032061fcf3b2bc3e7a6e53701be2c1
     )
 }
 
