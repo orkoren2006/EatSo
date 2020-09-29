b5877729505b108ab20c6f54f011353f37d463be
Liel Malihi <lielm1995@gmail.com>
2020-09-29 12:07:28 +0300

    gb

--- backend/server.js
+++ backend/server.js
@@ -43,9 +43,9 @@ app.use('/api/booking', bookingRoutes)
 app.use('/api/exp', expRoutes)
 connectSockets(io)
 
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// })
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030;
